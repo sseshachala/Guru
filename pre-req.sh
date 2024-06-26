dfffcd8ffacc5def6662df1b7067b7aa97c27c50
Sudhi Seshachala <sudhi.seshachala@thomsonreuters.com>
2024-06-25 23:01:49 -0500

    added apt install python3-pip

--- pre-req.sh
+++ pre-req.sh
@@ -24,6 +24,7 @@ sudo apt install -y software-properties-common
 sudo add-apt-repository -y ppa:deadsnakes/ppa
 sudo apt update
 sudo apt install -y python3.12 python3.12-venv python3.12-dev
+apt install python3-pip
 sudo apt install alembic
 
 
